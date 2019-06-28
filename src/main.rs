7255fece654c7f49aa7412992beb06cf16b47dec
Christopher Johnstone <meson800@gmail.com>
2019-06-28 17:49:16 -0400

    First initial commit with a working EFI binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use r_efi::efi;
+
+// Infinite loop on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[export_name = "efi_main"]
+pub extern fn efi_main(handle: efi::Handle, sys_table: *mut efi::SystemTable) -> efi::Status {
+    efi::Status::SUCCESS
 }
